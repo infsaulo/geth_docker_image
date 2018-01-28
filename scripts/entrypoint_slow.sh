5795970957764b104433c292d1c9fd9ebc7f2bd6
infsaulo <infsaulok@gmail.com>
2018-01-27 19:47:04 -0800

    slow client set to the main network

--- scripts/entrypoint_slow.sh
+++ scripts/entrypoint_slow.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-/geth init /root/ropsten/metadata/genesis.json 
-/geth --networkid 3 --rpc --rpcaddr "0.0.0.0" --rpcapi eth,web3,debug --ws --wsaddr "0.0.0.0" --wsorigins "*" --wsapi eth,web3,debug --cache=1024
+/geth --networkid 1 --rpc --rpcaddr "0.0.0.0" --rpcapi eth,web3,debug --ws --wsaddr "0.0.0.0" --wsorigins "*" --wsapi eth,web3,debug --cache=1024
