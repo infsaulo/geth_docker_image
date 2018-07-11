78163c2d721d46c16283a8c42a0445befd652a02
infsaulo <infsaulok@gmail.com>
2018-07-11 12:56:52 -0700

    Refactoging entrypoint_slow.sh

--- scripts/entrypoint_slow.sh
+++ scripts/entrypoint_slow.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/geth --networkid 1 --rpc --rpcaddr "0.0.0.0" --rpcapi eth,web3,debug --ws --wsaddr "0.0.0.0" --wsorigins "*" --wsapi eth,web3,debug --cache=1024
+/geth --networkid 1 --syncmode "full" --gcmode "archive" --rpc --rpcaddr "0.0.0.0" --rpcapi eth,web3,debug --ws --wsaddr "0.0.0.0" --wsorigins "*" --wsapi eth,web3,debug --cache=1024
